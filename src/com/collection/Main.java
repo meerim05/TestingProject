bb0381b5cc858f99e1c93555d981489c21837ce8
meerim05 <meerimt005@gmail.com>
2021-08-15 19:48:22 -0400

    My commit

---
+++ src/com/collection/Main.java
@@ -0,0 +1,10 @@
+package com.collection;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i <3 ; i++) {
+            System.out.println("Testing");
+            Thread.sleep(2000);
+        }
+    }
+}
